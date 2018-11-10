1d72cdc66cfbbac340d13287a1b5586e592b5bf0
Thomas Herve <thomas.herve@etu.univ-grenoble-alpes.fr>
2018-11-10 15:25:52 +0100

    ajout file en c

---
+++ lib/c/file/file.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "file.h"
+
+File nouvelleFile(element_t donnee) {
+    File file = (File) malloc(sizeof(struct head_t));
+    file->head = nouvelleCellule(donnee);
+    file->tail = file->head;
+
+    return file;
+}
+
+Element nouvelleCellule(element_t donnee) {
+    Element e = (Element) malloc(sizeof(struct cellule_t));
+    e->donnee = donnee;
+    e->suivant = NULL;
+
+    return e;
+}
+
+int estVide(File file){
+    return(file->head == NULL);
+}
+
+void enfiler(File file,element_t donnee){
+    Element f = nouvelleCellule(donnee);
+    if(estVide(file)){
+        file->head = file->tail = nouvelleCellule(donnee);
+        return;
+    }
+    file->tail->suivant = f;
+    file->tail = f;
+}
+
+element_t defiler(File file){
+    if(file->head == file->tail){
+        element_t retour = file->head->donnee;
+        free(file->head);
+        file->head = file->tail = NULL;
+        return retour;
+    }
+
+    Element temp = file->head;
+    element_t retour = temp->donnee;
+    file->head = file->head->suivant;
+    return retour;
+}
+
+void detruire(File file) {
+    while (!estVide(file)) {
+        defiler(file);
+    }
+    free(file);
+}
+
+/* EXEMPLE D'UTILISATION
+int main() {
+    File file = nouvelleFile(10);
+
+    enfiler(file, 20);
+    enfiler(file, 30);
+
+    while(!estVide(file)){
+        printf("%d defiler\n", defiler(file));
+    }
+
+    enfiler(file, 30);
+    detruire(file);
+
+    return 0;
+}
+*/
---
+++ lib/c/file/file.h
@@ -0,0 +1,21 @@
+#ifndef FILE_H
+    #define FILE_H
+    #define element_t int
+
+    struct cellule_t {
+        element_t donnee;
+        struct cellule_t *suivant; 
+    };
+    typedef struct cellule_t *Element;
+    struct head_t {
+      Element head;
+      Element tail;  
+    };
+    typedef struct head_t *File;
+    
+    File nouvelleFile(element_t);
+    Element nouvelleCellule(element_t);
+    int estVide(File);
+    void enfiler(File,element_t);
+    element_t defiler(File);
+#endif
